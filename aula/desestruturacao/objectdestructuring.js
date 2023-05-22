7334801ea37757c4bc454db0d6a605893701add5
Gabriela Pimentel <78750796+gabifp@users.noreply.github.com>
2023-05-22 18:41:17 -0300

    Inclui novo exemplo de desestruturação

--- aula/desestruturacao/objectdestructuring.js
+++ aula/desestruturacao/objectdestructuring.js
@@ -16,6 +16,13 @@ const meu_nome = pessoa.nome;
 // No caso abaixo eu estou informando pelo nome da chave o valor que quero do objeto pessoa
 const { nome } = pessoa
 
+// teste de entendimento:
+const { endereco: { rua, numero }, endereco} = pessoa;
+
+console.log('O objeto endereço: ', endereco);
+console.log('As propriedades do endereço: 1 - ', rua);
+console.log('As propriedades do endereço: 2 - ', numero);
+
 // Nesse caso eu estou usando o nome da chave e dando um novo nome a variável que receberá o valor da chave
 const { sobrenome: meu_sobrenome, endereco: {rua: minha_rua, numero: meu_numero } } = pessoa
 
